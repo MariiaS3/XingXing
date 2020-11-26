4e62c5fe90cd51b5a90baaf1f22fcf7e436bc3a4
Klaudia2365 <72575178+Klaudia2365@users.noreply.github.com>
2020-11-26 23:19:51 +0100

    HUD-coins

---
+++ XingXing/HUD.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+
+var coins = 0
+
+func _ready():
+	$coins.text = String(coins)
+
+
+
+func _on_coin_collected():
+	coins = coins + 1
+	_ready()
--- XingXing/coin.gd
+++ XingXing/coin.gd
@@ -1,4 +1,8 @@
 extends Area2D
 
+signal coin_collected
+
 func _on_coin_body_entered(body):
 	queue_free()
+	emit_signal("coin_collected")
+	set_collision_mask_bit(0, false)
