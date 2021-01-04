7b2123f1d68b281b3aa15db3ff6e51e3be1396dd
Klaudia2365 <72575178+Klaudia2365@users.noreply.github.com>
2021-01-04 12:37:05 +0100

    Revert "enemy1"
    This reverts commit 2280af84cdeb84a2a59fe1fea9f7336433a58349.

--- XingXing/Enemy.gd
+++ XingXing/Enemy.gd
@@ -1,21 +1,20 @@
 extends KinematicBody2D
 
 var velocity = Vector2()
-export var direction = 1
+export var direction = -1
 
 func _ready():
 	if direction == 1:
-		$AnimatedSprite.flip_h = true
-
+		$AnimatedSprite.flip_h = true;
+		
 func _physics_process(delta):
 	if is_on_wall():
 		direction = direction * -1
 		$AnimatedSprite.flip_h = not $AnimatedSprite.flip_h
-	
-	velocity.y += 10
-	
-	velocity.x = 50 * direction
-
-	velocity = move_and_slide(velocity, Vector2.UP)
+		
+		velocity.y += 20
+		velocity.x = 50 * direction
+		
+		velocity = move_and_slide(velocity, Vector2.UP)
 		
 
