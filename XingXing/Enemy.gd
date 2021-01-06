cd1bbe54ac774c8fc029abf96963b6bdbed36c6c
Klaudia2365 <72575178+Klaudia2365@users.noreply.github.com>
2021-01-06 21:12:40 +0100

    enemy-poprawki

--- XingXing/Enemy.gd
+++ XingXing/Enemy.gd
@@ -5,13 +5,16 @@ export var direction = -1
 
 func _ready():
 	if direction == 1:
-		$AnimatedSprite.flip_h = true;
+		$AnimatedSprite.flip_h = true
+		
+	$RayCast2D.position.x = $CollisionShape2D.shape.get_extents().x * direction
 		
 func _physics_process(delta):
 	
 	if is_on_wall():
 		direction = direction * -1
 		$AnimatedSprite.flip_h = not $AnimatedSprite.flip_h
+		$RayCast2D.position.x = $CollisionShape2D.shape.get_extents().x * direction
 		
 	velocity.y += 20
 	velocity.x = 50 * direction
