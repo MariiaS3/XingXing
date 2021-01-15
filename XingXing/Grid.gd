dc83f70e4cddf5aba9240898323f7e08d2835271
MariiaS3 <you@example.com>
2021-01-15 12:53:32 +0100

    dodalam XD

---
+++ XingXing/Grid.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+var rndX 
+var rndY
+
+func _ready():
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	var rndX = rng.randi_range(300, 50)
+	var rndY = rng.randi_range(50, 50)
+	$Wiesielec.position = Vector2(rndX, rndY)
+	$Timer.connect("timeout",self,"_on_Timer_timeout()")
+
+func _on_Timer_timeout():
+	$Wiesielec.visible = !$Wiesielec.visible
+	_ready()
+
