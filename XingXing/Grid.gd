673f4363b029634b6ec280619792192228282f2a
Klaudia2365 <72575178+Klaudia2365@users.noreply.github.com>
2021-01-16 19:42:59 +0100

    .

--- XingXing/Grid.gd
+++ XingXing/Grid.gd
@@ -5,13 +5,13 @@ var rndX
 var rndY
 
 func _ready():
-	if PlayerData.quize > 0:
+	if Global.quiz > 0:
 		$Wiesielec/CollisionShape2D.disabled =true
 		queue_free()
 	var rng = RandomNumberGenerator.new()
 	rng.randomize()
-	var rndX = rng.randi_range(300, 50)
-	var rndY = rng.randi_range(50, 50)
+	var rndX = rng.randi_range(200, 50)
+	var rndY = rng.randi_range(30, 30)
 	$Wiesielec.position = Vector2(rndX, rndY)
 	$Timer.connect("timeout",self,"_on_Timer_timeout()")
 
@@ -21,5 +21,4 @@ func _on_Timer_timeout():
 
 
 func _on_Wiesielec_body_entered(body):
-	PlayerData.quize+=1
-	
+	Global.quiz+=1
