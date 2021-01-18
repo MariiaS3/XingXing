59e00c10e056debd7339133c4227f13961e55598
Klaudia2365 <72575178+Klaudia2365@users.noreply.github.com>
2021-01-18 14:43:30 +0100

    Revert "."
    This reverts commit 3255feca49987ad69b674d3132ad28793a048022.

--- XingXing/Grid.gd
+++ XingXing/Grid.gd
@@ -9,17 +9,14 @@ func _ready():
 	rng.randomize()
 	rndX = rng.randi_range(200, 50)
 	rndY = rng.randi_range(30, 30)
-	$Sudoku.position = Vector2(rndX, rndY)
-	PauseTime.connect("timeout",$Sudoku,"_on_Timer_timeout()")
+	$Wiesielec.position = Vector2(rndX, rndY)
+	PauseTime.connect("timeout",$Wiesielec,"_on_Timer_timeout()")
 
 func _on_Timer_timeout():
-	$Sudoku.visible = !$Sudoku.visible
-	if PlayerData.sudok > 0:
-		$Sudoku/CollisionShape2D.disabled =true
+	$Wiesielec.visible = !$Wiesielec.visible
+	if PlayerData.quize > 0:
+		$Wiesielec/CollisionShape2D.disabled =true
 		queue_free()
 
-
-
-
-func _on_Sudoku_body_entered(body):
-	PlayerData.sudok+=1
+func _on_Wiesielec_body_entered(_body):
+	PlayerData.quize+=1
